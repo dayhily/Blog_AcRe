4902dd82c2c152927ab07f069cd775436a2dcbe7
Yura <yura@whatever.com>
2017-04-02 11:02:57 +0300

    Remake Blog to ActiveRecord

--- app.rb
+++ app.rb
@@ -1,34 +1,26 @@
+#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'sinatra/activerecord'
 
-def init_db
-  @db=SQLite3::Database.new 'testblog.db' #Инициализация БД ввиде массива
-  @db.results_as_hash=true #Результат инициализации ввиде хэша, (ключ-название поля, значение-данные поля)
+#Устанавливаем соединение с БД barbershop.db
+set :database, "sqlite3:public/posts.db"
+
+#Cоздание модели БД в классе Post
+class Post < ActiveRecord::Base
+  has_many :comments
+	validates :content, presence: true
 end
 
-before do #Вызывается перед каждым запросом (перезагрузкой любой страницы)
-  init_db #Инициализация БД
-end
-
-configure do #метод конфигурации приложения вызывается каждый раз при инициализации приложения
-             #инициализация происходит при сохранении, изменении файла и обновлении страницы
-  init_db #Инициализация БД
-  @db.execute 'CREATE TABLE IF NOT EXISTS `posts` (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`created_date`	DATE,
-	`content`	TEXT)' #создание таблицы в БД если ее нет
-  
-   @db.execute 'CREATE TABLE IF NOT EXISTS `comments` (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`post_id` INTEGER,
-	`created_date`	DATE,
-	`content`	TEXT)' #создание таблицы в БД если ее нет
+class Comment < ActiveRecord::Base
+	belongs_to :post
+	validates :content, presence: true
 end
 
 get '/' do  
-  @results = @db.execute 'SELECT * FROM posts ORDER BY ID DESC' #выбор списка постов из БД (ввиде хэша)
+  @results = Post.order "id DESC" #выбор списка постов из БД (ввиде хэша)
   erb :index
 end
 
@@ -37,50 +29,55 @@ get '/new' do #обработчик get запроса /new (формирует 
 end
 
 post '/new' do #обработчик post запроса /new
-  post_content=params[:content] #получаем переменную(параметр) content из post запроса
-  
-  if post_content.length <= 0 #если переменная post_content пустая, то
-    @error = 'Type text'# @error присваивается сообщение об ошибке
-    return erb :new #и возвращается представление /new
+  #Принимаем данные со страницы /visit (ActiveRecird)
+  @c=Post.new params[:pst]
+    
+  #Сохраняем данные в таблицу, если прошли валидацию
+  if @c.save
+	  erb "<h2>Thanks, You have been added!</h2>"
+  else
+	  #Иначе выводит первый элемент из массива ошибок
+	  @error = @c.errors.full_messages.first
+	  erb :new
   end
+end
 
-  #сохранение переменных content и created_date со значениями [post_content] и datetime() в таблицу posts
-  @db.execute 'INSERT INTO
-    posts (content, created_date)
-       values (?, datetime() )', [post_content] 
-  
-  redirect to '/' #перенаправление на главную страницу после отправки формы
+delete '/del' do
+  postdel = Post.find(params[:id])
+  postdel.destroy
+  redirect '/'
 end
 
 get '/comment/:post_id' do #обработчик get запроса для url`а (формирует страницы)
-  post_id=params[:post_id] #браузер получает параметр(название страницы) из url`а
-  #для каждой ссылки добавляется id поста считанный из url`а.
-  result = @db.execute 'SELECT * FROM posts WHERE id = ?', [post_id] #выбор списка постов из БД c определенным id (ввиде хэша)
-  @row=result[0] #выбор строки с индексом 0 из списка в переменную @row (т.е. самый первый пост)
-  #выбираем комментарии из БД для поста по post_id
-  @comments = @db.execute 'SELECT * FROM comments WHERE post_id = ? ORDER BY id', [post_id]
-  erb :comments #возвращаем представление comments.erb
+  #получаем переменную из url`а
+  post_id = params[:post_id]
+  
+  #получаем список постов - у нас будет только один пост по этому номеру
+  @result = Post.find(params[:post_id])
+  
+  #выбираем комментарии для нашего поста
+	#@comments = Comment.find_by post_id: post_id
+	@comments = Comment.all.where post_id: post_id
+  
+  erb :comments
 end
 
 post '/comment/:post_id' do #обработчик post запроса для url`а
-	post_id=params[:post_id] #браузер получает параметр(название страницы) из url`а
-  post_content=params[:content] #получаем переменную(параметр) content из post запроса
+	#получаем переменную из url`а
+  post_id = params[:post_id]
   
-  result = @db.execute 'SELECT * FROM posts WHERE id = ?', [post_id] #выбор списка постов из БД c определенным id (ввиде хэша)
-  @row=result[0] #выбор строки с индексом 0 из списка в переменную @row (т.е. самый первый пост)
-  #выбираем комментарии из БД для поста по post_id
-  @comments = @db.execute 'SELECT * FROM comments WHERE post_id = ? ORDER BY id', [post_id]
-    
-  if post_content.length <= 0 then #если переменная post_content пустая, то
-      #@error присваивается сообщение об ошибке и возвращается представление /comments
-		  (erb @error = 'Type comment' and return erb :comments) 
-    redirect to ('/comment/' + post_id) #redirect на эту же страницу
-  end
+	# получаем содержание комментария и записываем в переменную БД
+	@d = Comment.new params[:cmt]
+	# добавляем в переменную номер поста из параметров урл-а
+	@d.post_id = post_id
   
-  #сохранение переменных в таблицу comments
-  @db.execute 'INSERT INTO
-    comments (post_id, content, created_date)
-      values (?, ?, datetime() )', [post_id, post_content] 
-  erb :comments
-  redirect to ('/comment/' + post_id)
+  #Сохраняем данные в таблицу, если прошли валидацию
+    if @d.save
+			redirect to ('/comment/' + post_id)
+  	else
+  		#Иначе выводит первый элемент из массива ошибок
+		  @error = @d.errors.full_messages.first
+		  erb :comments
+		end
 end
+
